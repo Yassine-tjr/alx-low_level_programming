e892550810b9b9cf4b8ec148d94e48381f2aca90
Yassine <toujriyassine@hotmail.fr>
2023-06-15 13:40:23 -0700

    de

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
-#include <stdio.>
+#include <stdio.h>
 /**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
+ * main - 5-printf.c
+ * description - Write a C program that prints exactly with proper grammar,
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art, \n");
-return(0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+
+        return (0);
 }
